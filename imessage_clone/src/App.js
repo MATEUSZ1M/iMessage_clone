2b112ec47aa64a85db325452ac662d56ba617d71
Mateusz Mikolajczyk <mikolajczykmateusz1993@gmail.com>
2021-01-25 19:04:11 +0100

    Prepare file structure for sidebar component

--- imessage_clone/src/App.js
+++ imessage_clone/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Imessage from './Components/Imessage/Imessage';
 
 function App() {
   return (
     <div className="app">
       <h1>iMesage</h1>
+      <Imessage/>
     </div>
   );
 }
---
+++ imessage_clone/src/Components/Imessage/Imessage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Imessage.css';
+import Sidebar from '../Sidebar/Sidebar';
+
+
+function Imessage() {
+  return (
+    <div className='imessage'>
+      {/* sidebar */}
+      <Sidebar />
+      {/* chat */}
+    </div>
+  )
+}
+
+export default Imessage
