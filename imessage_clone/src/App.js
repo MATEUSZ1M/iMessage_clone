defa861d1ad6dcd6ca1c9ac6ffd1339eea3696e1
Mateusz Mikolajczyk <mikolajczykmateusz1993@gmail.com>
2021-01-25 19:42:45 +0100

    Add sidebar header

--- imessage_clone/src/App.js
+++ imessage_clone/src/App.js
@@ -5,7 +5,6 @@ import Imessage from './Components/Imessage/Imessage';
 function App() {
   return (
     <div className="app">
-      <h1>iMesage</h1>
       <Imessage/>
     </div>
   );
--- imessage_clone/src/Components/Sidebar/Sidebar.js
+++ imessage_clone/src/Components/Sidebar/Sidebar.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
+import "./Sidebar.css";
+import Avatar from "@material-ui/core/Avatar";
+import SearchIcon from "@material-ui/icons/Search";
+import RateReviewOutlinedIcon from "@material-ui/icons/RateReviewOutlined";
+import { IconButton } from "@material-ui/core";
 
 function Sidebar() {
   return (
-    <div className='sidebar'>
-      <h2>I am the sidebar</h2>
+    <div className="sidebar">
+      <div className="sidebar__header">
+        <Avatar className="sidebar__avatar" />
+        <div className="sidebar__input">
+          <SearchIcon />
+          <input type="text" placeholder="Search" />
+        </div>
+        <IconButton className="sidebar__inputButton" variant="outlined">
+          <RateReviewOutlinedIcon  />
+        </IconButton>
+      </div>
+      <div className="sidebar__chats"></div>
     </div>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
