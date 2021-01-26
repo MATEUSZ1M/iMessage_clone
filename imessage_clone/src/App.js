f96d5b655f3ee9e48083c07984a859ace0601b24
Mateusz Mikolajczyk <mikolajczykmateusz1993@gmail.com>
2021-01-26 16:54:22 +0100

    Add login funcionality

--- imessage_clone/src/App.js
+++ imessage_clone/src/App.js
@@ -1,19 +1,35 @@
-import React from 'react';
-import {useSelector} from "react-redux"
-import './App.css';
-import Imessage from './Components/Imessage/Imessage';
-import { selectUser } from './features/userSlice';
+import React, { useEffect } from "react";
+import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { selectUser, login, logout } from "./features/userSlice";
+import Imessage from "./Components/Imessage/Imessage";
+import Login from "./Components/Login/Login";
+import { auth } from "./features/firebase";
 
 function App() {
   const user = useSelector(selectUser);
+  const dispatch = useDispatch();
 
-  return (
-    <div className="app">
-      {user ? (
-        <Imessage/>
-      ): <h2>You need to login</h2>}
-    </div>
-  );
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        //user is logged in
+        dispatch(
+          login({
+            uid: authUser.uid,
+            photo: authUser.photoURL,
+            email: authUser.email,
+            displayName: authUser.displayName,
+          })
+        );
+      } else {
+        //user i logged out
+        dispatch(logout());
+      }
+    });
+  }, []);
+
+  return <div className="app">{user ? <Imessage /> : <Login />}</div>;
 }
 
 export default App;
