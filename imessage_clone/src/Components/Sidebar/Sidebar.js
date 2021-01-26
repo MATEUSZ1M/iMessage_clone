8e2e62ac50d8637183063d4cf309b190499ff2be
Mateusz Mikolajczyk <mikolajczykmateusz1993@gmail.com>
2021-01-26 17:12:16 +0100

    Add logOut funcionality

--- imessage_clone/src/Components/Sidebar/Sidebar.js
+++ imessage_clone/src/Components/Sidebar/Sidebar.js
@@ -5,12 +5,21 @@ import Avatar from "@material-ui/core/Avatar";
 import SearchIcon from "@material-ui/icons/Search";
 import RateReviewOutlinedIcon from "@material-ui/icons/RateReviewOutlined";
 import { IconButton } from "@material-ui/core";
+import { useSelector } from "react-redux";
+import { selectUser } from "../../features/userSlice";
+import { auth } from "../../features/firebase";
 
 function Sidebar() {
+  const user = useSelector(selectUser);
+
   return (
     <div className="sidebar">
       <div className="sidebar__header">
-        <Avatar className="sidebar__avatar" />
+        <Avatar
+          src={user.photo}
+          onClick={() => auth.signOut()}
+          className="sidebar__avatar"
+        />
         <div className="sidebar__input">
           <SearchIcon />
           <input type="text" placeholder="Search" />
